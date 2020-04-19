7cc64028227d53e2e0294a55a4b585f2bf9a1d96
Lucas Souza <lucassouza690@gmail.com>
2020-04-19 17:08:45 -0300

    Aula  1 2 3 criacao WCF basico

---
+++ Passagens/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passagens
+{
+  public class Cliente
+  {
+    [DataMember]
+    public string Nome { get; set; }
+    [DataMember]
+    public string Cpf { get; set; }
+  }
+}
---
+++ Passagens/ClienteService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passagens
+{
+  public class ClienteService : IClienteService
+  {
+    public void Add(Cliente c)
+    {
+      ClienteDao dao = new ClienteDao();
+      dao.Add(c);
+    }
+
+    public Cliente Buscar(string nome)
+    {
+      ClienteDao dao = new ClienteDao();
+      return dao.Buscar(nome);
+      
+    }
+  }
+}
